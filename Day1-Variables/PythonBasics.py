94c38292663fc58258d8345afeb673cb58a08b91
jaan18 <jovas28@hotmail.com>
2021-04-19 23:20:24 -0500

    First day exercises and project

---
+++ Day1-Variables/PythonBasics.py
@@ -0,0 +1,33 @@
+# Imprimir en consola
+# print("Hello World!")
+#control+k control+c comentar linea
+
+# Day 1-1 Exercise
+""" print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')") """
+
+# Python input() function
+#input("A prompt for the user")
+
+# Day 1-3 Exercise 
+#Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string. 
+""" name = input("Introduce tu nombre para ver cuantos caracteres tiene ")
+print(f"Tu nombre es {name} y tiene {len(name)} caracteres") """
+
+# Day 1-4 Exercise
+# Write a program that switches the values stored in the variables a and b.
+""" a = input("a: ")
+b = input("b: ")
+ """
+""" aTemporal = b
+bTemporal = a
+a = aTemporal
+b = bTemporal """
+
+""" c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b) """
---
+++ Day1-Variables/band-name-generator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print("Hello to my band name generator!")
+city = input("What city did you grow up?\n ")
+namePet = input("What is the name of your pet?\n ")
+
+print(f"Your new band name is {city} {namePet}")
